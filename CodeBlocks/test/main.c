24a47206b03ec53fe290ae29fb6d5f428fc11ad8
hahawang521 <47842045+hahawang521@users.noreply.github.com>
2020-04-06 13:31:05 +0800

    TEST
    解密计算器

--- CodeBlocks/test/main.c
+++ CodeBlocks/test/main.c
@@ -35,58 +35,44 @@ int main()
 	i_Buf[5]=2020;
     time=60;
 
-    day = diff(1900,1,1,2030,4,1);
-    //printf("%d\n",day);
-    //for(int i =1;i<30;i++)
+
+    printf("输入注册码:");
+    scanf("%d",&code);
+    printf("注册码为%d\n",code);
+
+    printf("输入再加密时间:");
+    scanf("%d",&time);
+    //printf("再加密时间为%d\n",time);
+
+    //计算解密码
+    unlock[0] = (code%1000)^769;                              //源加密天数
+    unlock[1] = (code/1000)^99699;                            //源加密日期
+    unlock[2] = (((unlock[1]+unlock[0]*3+5)^99999))%10000;    //解锁密码--取计算值后4位
+
+    unlock[3] = ((unlock[2])%10000)*10000+(time^341)+2371;
+    printf("解锁码为%d\n",unlock[3]);
+
+    printf("输入解密码:");
+    scanf("%d",&incode);
+    printf("解密码为%d\n",incode);
+
+    //提取输入值的前4位
+    unlock[4] = ((incode/10000))%10000;
+    //提取输入值的后4位
+    unlock[5] = ((incode%10000-2371)^341);                    //还原天数
+
+    //若解密码正确
+    if(unlock[2] == unlock[4])
     {
-        //for(int j=5;j<=100;j+=5)
-        {
 
-            //untime=j;
-            //i_Buf[3]=i;
-            day1 = diff(1900,1,1,i_Buf[5],i_Buf[4],i_Buf[3]);
-            //计算注册码
-            tmp_day = (day1 ^ 99699)%100000;
-            tmp_time = (time + i_Buf[3]) ^ 341;
-            tmp_time =  (tmp_time%1000);
-            code=tmp_day*1010+tmp_time;
-            printf("注册码为%d\n",code);
-
-            printf("输入注册码:");
-            scanf("%d",&code);
-            printf("注册码为%d\n",code);
-
-            printf("输入再加密时间:");
-            scanf("%d",&time);
-            printf("再加密时间为%d\n",time);
-            //计算解密码
-            unlock[0]=(code%1000)^769;                              //源加密天数
-            unlock[1]=(code/1000)^99699;                            //源加密日期
-            unlock[2]=(((unlock[1]+unlock[0]*3+5)^99999))%10000;    //解锁密码--取计算值后4位
-            unlock[3]=((unlock[2]^9647)%10000)*10000+(time^341)+2371;
-            printf("解锁码为%d\n",unlock[3]);
-
-            printf("输入解密码:");
-            scanf("%d",&incode);
-            printf("注册码为%d\n",incode);
-
-            //提取输入值的前4位
-            unlock[4]=((incode/10000))%10000;
-            //提取输入值的后4位
-            unlock[5]=((incode%10000-2371)^341);                    //还原天数
-
-            //若解密码正确
-            if(unlock[2] == unlock[4])
-            {
-
-                printf("解锁码正确\n");
-            }
-            else
-            {
-                printf("解锁码错误\n");
-            }
-        }
+    printf("解锁码正确,加密天数为%d\n",unlock[5]);
     }
+    else
+    {
+    printf("解锁码错误\n");
+    }
+
+
 
 
     while(1);
